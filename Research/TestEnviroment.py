f46c804ffd1ceb908fb6d18abe94cdda18fbfb1c
Alex-D-G <60549983+Alex-D-G@users.noreply.github.com>
2021-06-14 15:42:26 +0200

    Update TestEnviroment.py

--- Research/TestEnviroment.py
+++ Research/TestEnviroment.py
@@ -90,31 +90,23 @@ def heuristic(current_node, target_node):
         9: (17.5, 0), 10: (19.5, 6), 11: (19.5, 19), 12: (25.5, 6), 13: (25.5, 19), 14: (25.5, 25), 15: (42.5, 6),
         16: (42, 19), 17: (10.5, 17), 18: (45.5, 19), 19: (72, 15.5), 20: (48, 19), 21: (56, 19), 22: (56, 25),
         23: (62, 6), 24: (62, 19), 25: (64, 0), 26: (64, 6), 27: (66, 19), 28: (66, 25), 29: (72, 0),
-        30: (72, 6), 31: (72, 19)
+        30: (72, 6), 31: (72, 19), 32: (74.5, 15.5), 33: (74.5, 19)
     }
     cordsX = (nodeCords[current_node][0] - nodeCords[target_node][0])
     cordsY = (nodeCords[current_node][1] - nodeCords[target_node][1])
     return math.sqrt((cordsX * cordsX) + (cordsY * cordsY))
 
 
-def get_minimum_f(unvisited, graph, target):
+def get_minimum_f(unvisited):
     minimum_key = list(unvisited.keys())[0]
-    i = 0
     for key in unvisited.keys():
-        if(len(graph[key][1])) > 1:
-            minimum_key = list(unvisited.keys())[i]
-        i += 1
-    for key in unvisited.keys():
-        if target in graph[key][1]:
-            minimum_key = key
-        elif unvisited[key][1] < unvisited[minimum_key][1] and len(graph[key][1]) > 1:
+        if unvisited[key][1] < unvisited[minimum_key][1]:
             minimum_key = key
 
     return minimum_key
 
 
 def a_star(graph, start_node, target_node):
-    route = []
     visited = {}
     unvisited = {}
     distance = 0
@@ -126,11 +118,12 @@ def a_star(graph, start_node, target_node):
     unvisited[start_node] = [0, h_score, None]
 
     finished = False
-    current_node = start_node
     while not finished:
         if len(unvisited) == 0:
             finished = True
         else:
+            current_node = get_minimum_f(unvisited)
+            distance = unvisited[current_node][0]
             if current_node == target_node:
                 finished = True
                 visited[current_node] = unvisited[current_node]
@@ -138,27 +131,19 @@ def a_star(graph, start_node, target_node):
                 for neighbour in graph[current_node][1].keys():
                     if neighbour not in visited:
                         new_g_score = unvisited[current_node][0] + graph[current_node][1][neighbour]
-                        unvisited[neighbour][0] = new_g_score
-                        unvisited[neighbour][1] = new_g_score + heuristic(neighbour, target_node)
-                        unvisited[neighbour][2] = current_node
+                        if new_g_score < unvisited[neighbour][0]:
+                            unvisited[neighbour][0] = new_g_score
+                            unvisited[neighbour][1] = new_g_score + heuristic(neighbour, target_node)
+                            unvisited[neighbour][2] = current_node
                 visited[current_node] = unvisited[current_node]
-                route.append(current_node)
                 del unvisited[current_node]
 
-            neighbours = {}
-            for neighbour_key in graph[current_node][1].keys():
-                if neighbour_key in unvisited:
-                    neighbours[neighbour_key] = unvisited[neighbour_key]
-            if target_node in neighbours:
-                current_node = target_node
-            else:
-                if current_node != target_node:
-                    current_node = get_minimum_f(neighbours, graph, target_node)
-                else:
-                    finished = True
-            if unvisited[current_node][0] != float('inf'):
-                distance = unvisited[current_node][0]
-    route.append(target_node)
+    node = target_node
+    route = [node]
+    while node != start_node:
+        node = visited[node][2]
+        route.insert(0, node)
+
     return distance, route
 
 schoolLayout = {1: ("", {2:9}),
